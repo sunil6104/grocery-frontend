b314393cafa1e3067cfc9f10d3abbf10cae795cf
sunil6104 <sunilc1237493@gmail.com>
2025-03-15 14:39:28 +0530

    grocery-frontend

--- page.js
+++ page.js
@@ -1,31 +1,237 @@
-import Image from "next/image";
-import Header from "./_components/Header";
-import Banner from "./_components/Banner";
-import Category from "./_components/category/Category";
-import Product from "./product/page";
-import PaymentMethod from "./_components/PaymentMethod";
-import Newsletter from "./_components/Newsletter";
-import Footer from "./_components/Footer";
+import { Button } from '@/components/ui/button'
+import { HeartIcon } from '@heroicons/react/24/outline'
+import { ShoppingBag, ShoppingBagIcon, Star } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
+const Product
+ = () => {
   return (
     <>
-    <Header />
-    <Banner />
-    <PaymentMethod />
-    <Category />
-    <div className="container px-14 transition-transform">
-    <h1 className="text-4xl font-semibold py-9 px-14">Popular Product </h1>
-    <Product />
-    <Newsletter />
-    <Footer />
- 
-
-    </div>
- 
-
-    </>
+    <div className="container flex justify-center items-center ps-10 py-10">
+    <div className="grid grid-cols-4  gap-10 px-14">
     
-  );
-  
+      <div className="Product_col min-h-80  w-full max-w-48 rounded-xl shadow-xl">
+      <div className="bg-gray-100 mb-3 p-4">
+         <Image
+            src="/asset/basket1.png" 
+         alt="basket1/1" 
+         className="cursor-pointer"
+          width={1000} height={50} />
+          </div>
+          <div className="flex items-center py-2 px-1">
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+				</div>
+
+           <div className="grid grid-cols-2 px-2 py-2 items-center">
+             <h2 className="text-1x1">Vegtable</h2>
+             <h2 className="text-end font-semibold text-lg">₹500</h2>
+           </div>
+           <div className="grid grid-cols-1 px-2 py-2 items-center">
+           <Button className="bg-green-600">Add To Cart <ShoppingBagIcon className="ps-0"/></Button>
+             <a className="flex justify-end">
+             <HeartIcon width={25} className="hover:text-red-600 cursor-pointer"/>
+             </a>
+           </div>
+        </div>{" "}
+
+		<div className="Product_col min-h-80  w-full max-w-48 rounded-xl shadow-xl">
+      <div className="bg-gray-100 mb-3 p-4">
+         <Image
+            src="/asset/basket1.png" 
+         alt="basket1/1" 
+         className="cursor-pointer"
+          width={1000} height={50} />
+          </div>
+          <div className="flex items-center py-2 px-1">
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+				</div>
+
+           <div className="grid grid-cols-2 px-2 py-2 items-center">
+             <h2 className="text-1x1">Vegtable</h2>
+             <h2 className="text-end font-semibold text-lg">₹500</h2>
+           </div>
+           <div className="grid grid-cols-1 px-2 py-2 items-center">
+           <Button className="bg-green-600">Add To Cart <ShoppingBagIcon className="ps-0"/></Button>
+             <a className="flex justify-end">
+             <HeartIcon width={25} className="hover:text-red-600 cursor-pointer"/>
+             </a>
+           </div>
+        </div>{" "}
+
+		<div className="Product_col min-h-80  w-full max-w-48 rounded-xl shadow-xl">
+      <div className="bg-gray-100 mb-3 p-4">
+         <Image
+            src="/asset/basket1.png" 
+         alt="basket1/1" 
+         className="cursor-pointer"
+          width={1000} height={50} />
+          </div>
+          <div className="flex items-center py-2 px-1">
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+				</div>
+
+           <div className="grid grid-cols-2 px-2 py-2 items-center">
+             <h2 className="text-1x1">Vegtable</h2>
+             <h2 className="text-end font-semibold text-lg">₹500</h2>
+           </div>
+           <div className="grid grid-cols-1 px-2 py-2 items-center ">
+           <Button className="bg-green-600">Add To Cart <ShoppingBagIcon className="ps-0"/></Button>
+             <a className="flex justify-end">
+             <HeartIcon width={25} className="hover:text-red-600 cursor-pointer"/>
+             </a>
+           </div>
+        </div>{" "}
+
+		<div className="Product_col min-h-80  w-full max-w-48 rounded-xl shadow-xl ">
+      <div className="bg-gray-100 mb-3 p-4">
+         <Image
+            src="/asset/basket1.png" 
+         alt="basket1/1" 
+         className="cursor-pointer"
+          width={1000} height={50} />
+          </div>
+          <div className="flex items-center py-2 px-1">
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<svg className="w-6 h-6 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+						</path>
+					</svg>
+					<span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+				</div>
+
+           <div className="grid grid-cols-2 px-2 py-2 items-center">
+             <h2 className="text-1x1">Vegtable</h2>
+             <h2 className="text-end font-semibold text-lg">₹500</h2>
+           </div>
+           <div className="grid grid-cols-1 px-2 py-2 items-center ">
+           <Button className="bg-green-600">Add To Cart <ShoppingBagIcon className="ps-0"/></Button>
+             <a className="flex justify-end">
+             <HeartIcon width={25} className="hover:text-red-600 cursor-pointer"/>
+             </a>
+           </div>
+        </div>{" "}
+
+        </div>
+        </div>
+    
+    </>
+  )
 }
+
+export default Product
